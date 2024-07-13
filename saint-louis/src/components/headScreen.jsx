0d5cea16b9b3290a7958ead87802b39f26b697e0
Melria <127540628+Melria@users.noreply.github.com>
2024-07-13 12:55:57 +0100

    top-page finished

--- saint-louis/src/components/headScreen.jsx
+++ saint-louis/src/components/headScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const HeadScreen=()=> {
   return (
-    <div className='headScreen' id='headScreen'>
+    <div className='headScreen' id='header'>
         <p className='p'>PAROISSE SAINT LOUIS DE BONABéRI</p>
         <p className='playfair-font'>"L'honneur et la vertu sont les seuls biens véritables<br/>
         que l'homme puisse posséder."</p>
